5bf650dfa710cb6a1a3987b77a7a9b14e8392bda
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2016-09-02 03:52:13 -0300

    fixing command to stop the server

--- scripts/stop-site2-eap6.sh
+++ scripts/stop-site2-eap6.sh
@@ -6,8 +6,7 @@ export EAP_HOME=@@rhq.deploy.dir@@/@@eap.folder.name@@
 
 echo ">> Stopping the EAP server bound to address: $IP and at management port: $MGMT_PORT "
 
-export REMOTE_SERVER_EXISTS=`nc -z $IP $MGMT_PORT | wc -l`
-if [[ $REMOTE_SERVER_EXISTS -ge 1 ]]; then 
+nc -z $IP $MGMT_PORT
+if (($? == 0)); then
     $EAP_HOME/bin/jboss-cli.sh --connect --controller=$IP:$MGMT_PORT --user=@@xsite.admin.username@@ --password=@@xsite.admin.password@@ --command="shutdown"
 fi
-
--- scripts/stop-site2-jdg-cluster.sh
+++ scripts/stop-site2-jdg-cluster.sh
@@ -5,9 +5,8 @@ export EAP_HOME=@@rhq.deploy.dir@@/@@eap.folder.name@@
 for OFFSET in $OFFSETS; do
     ((MGMT_PORT = 9999 + OFFSET))
     echo ">> Stopping the JDG server bound to address: $IP and at management port: $MGMT_PORT "
-    export REMOTE_SERVER_EXISTS=`nc -z $IP $MGMT_PORT | wc -l`
-    if [[ $REMOTE_SERVER_EXISTS -ge 1 ]]; then 
+    nc -z $IP $MGMT_PORT
+    if (($? == 0)); then
         $EAP_HOME/bin/jboss-cli.sh --connect --controller=$IP:$MGMT_PORT --user=@@xsite.admin.username@@ --password=@@xsite.admin.password@@ --command="shutdown"
     fi
 done
-
